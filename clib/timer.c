e6331dedaf62bbc4d6fd9e9e3b1b60db7cf53bee
Cass <kasull88@gmail.com>
2011-07-05 19:53:24 -0400

    Timer uses references to get the lua function to run; allows multiple timers to run now

--- clib/timer.c
+++ clib/timer.c
@@ -7,14 +7,15 @@
 #include "lualock.h"
 #include "timer.h"
 
-void timer_new(l_timer_t *timer, unsigned int int_us, int cycles, void (*cb)(void)) {
+void timer_new(l_timer_t *timer, unsigned int int_us, int cycles,
+               void (*cb)(void*)) {
     timer->cycles = cycles;
     timer->completed_cycles = 0,
     timer->running = TRUE;
     timer->int_us = int_us;
     timer->cb = cb;
     timer_run(timer);
-    int source_id = g_timeout_add(int_us / 1000.0, timer_run, timer);
+    timer->id = g_timeout_add(int_us, timer_run, timer);
 }
     
 gboolean timer_run(void *data) {
@@ -22,35 +23,36 @@ gboolean timer_run(void *data) {
     if (timer->cycles != 0 && timer->completed_cycles >= timer->cycles)
         return FALSE;
     else
-        timer->cb();
+        timer->cb(timer);
     timer->completed_cycles++;
     return TRUE;
 }
 
-void timer_run_lua_function() {
-    lua_getfield(lualock.L, LUA_REGISTRYINDEX, "callback");
-    lua_pcall(lualock.L, 0, 0, 0);
+static void timer_run_lua_function(void *data) {
+    l_timer_t *timer = data;
+    lua_rawgeti(timer->L, LUA_REGISTRYINDEX, timer->r);
+    lua_pcall(timer->L, 0, 0, 0);
 }
 
 static int lualock_lua_timer_new(lua_State *L) {
     l_timer_t *timer = lua_newuserdata(L, sizeof(l_timer_t));
     
-    int interval = luaL_checknumber(L, 2) * 1000000;
+    int interval = luaL_checknumber(L, 2) * 1000;
     int run_times = lua_tonumber(L, 3);
     lua_pop(L, 2);
-    lua_pushstring(L, "callback");
-    lua_insert(L, 1);
-    lua_settable(L, LUA_REGISTRYINDEX);
-        
+    
+    timer->L = L;
+    timer->r = luaL_ref(L, LUA_REGISTRYINDEX);
+    
     timer_new(timer, interval, run_times, timer_run_lua_function);
     return 1;
 }
 
 void lualock_lua_timer_init(lua_State *L) {
-        const struct luaL_reg lualock_timer_lib[] =
-        {
-                { "new", lualock_lua_timer_new },
-                { NULL, NULL }
-        };
-        luaL_register(L, "timer", lualock_timer_lib);
+    const struct luaL_reg lualock_timer_lib[] =
+    {
+        { "new", lualock_lua_timer_new },
+        { NULL, NULL }
+    };
+    luaL_register(L, "timer", lualock_timer_lib);
 }
--- clib/timer.h
+++ clib/timer.h
@@ -5,14 +5,18 @@
 #include <stdbool.h>
 
 typedef struct {
+    int id;
+    lua_State *L;
     bool running;
     int cycles;
     int completed_cycles;
     int int_us;
-    void (*cb)(void);
+    int r;
+    void (*cb)(void*);
 } l_timer_t;
 
-void timer_new(l_timer_t *timer, unsigned int int_us, int cycles, void (*cb)(void));
+void timer_new(l_timer_t *timer, unsigned int int_us, int cycles,
+               void (*cb)(void*));
 gboolean timer_run(void *data);
 
 void lualock_lua_timer_init(lua_State *L);
