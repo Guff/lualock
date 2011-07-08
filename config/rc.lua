c367a4c248f4aaa04b91f740eff6758098e5c499
Cass <kasull88@gmail.com>
2011-07-08 14:25:07 -0400

    Update sample config

--- config/rc.lua
+++ config/rc.lua
@@ -1,32 +1,38 @@
-style.set({ color = "#333333", font = "Sans 12", x = 500, y = 400, off_x = 5,
-            off_y = 6, width = 150, height = 24 })
+style({ color = "#333333", font = "Sans 12", x = 500, y = 400, off_x = 5,
+        off_y = 6, width = 150, height = 24 })
 
-background.set("color", "#ff0000")
-background.set("/usr/share/backgrounds/Aeg_by_Tauno_Erik.jpg", "zoom")
-im2 = image.new("/usr/share/archlinux/logos/archlinux-official-light.svg")
-image.rotate(im2, 60)
-image.scale(im2, 0.75, 0.75)
-image.render(im2)
+--dpms({ standby = 5, suspend = 100, off = 200 })
 
-user_text = text.new("User: " .. os.getenv("USER"), 500, 300, "Sans 16",
-                     "#666666")
-text.draw(user_text);
+background("color", "#ff0000")
+background("/usr/share/backgrounds/Aeg_by_Tauno_Erik.jpg", "zoom")
+im2 = image("/usr/share/archlinux/logos/archlinux-official-light.svg")
+im2:rotate(60)
+im2:scale(0.75, 0.75)
+im2:render()
 
-text_clock = text.new(os.date("%a %b %d, %l:%M:%S %p"), 0, 0, "Sans 16", "#000000")
-text.draw(text_clock)
-clock_timer = timer.new(function ()
-    text.set(text_clock, os.date("%a %b %d, %l:%M:%S %p"))
+user_text = text({ text = "User: " .. os.getenv("USER"), x = 500, y = 370,
+                  font = "Anton 16", color = "#ffffff", border_color = "#000000", border_width = 3 })
+user_text:draw()
+
+text_clock = text{ text = os.date("%a %b %d, %l:%M:%S %p"), x = 0, y = 0,
+                   font = "Sans 20", color = "#ffffff", border_color = "#000000", border_width = 4 }
+text_clock:draw()
+clock_timer = timer(function ()
+    text_clock:set({ text = os.date("%a %b %d, %l:%M:%S %p") })
+    text_clock:draw()
 end, 1)
 
-text_timer_text = text.new("Locked for 0 seconds", 100, 300, "Sans 12", "#006600")
-text.draw(text_timer_text)
+text_timer_text = text{ text = "Locked for 0 seconds", x = 100, y = 300, 
+                        font = "Sans 12", color = "#ffffff", border_color = "#000000", border_width = 6 }
+text_timer_text:draw()
 start_time = os.time()
-text_timer = timer.new(function ()
-	text.set(text_timer_text, string.format("Locked for %i seconds", os.time() - start_time))
+text_timer = timer(function ()
+	text_timer_text:set({ text = string.format("Locked for %i seconds", os.time() - start_time) })
+    text_timer_text:draw()
 end, 1)
 
 function rotate_image ()
-    image.rotate(im2, 3)
+    im2:rotate(3)
 end
 
-t = timer.new(rotate_image, 0.05);
+t = timer(rotate_image, 0.05);
