0ff7b931dbed7dbc1c84e10feb0de1179dfe2b32
Cass <kasull88@gmail.com>
2011-07-29 12:31:15 -0400

    Added method for resizing cairo surfaces

--- clib/cairo_surface.c
+++ clib/cairo_surface.c
@@ -11,9 +11,6 @@ static int lualock_lua_cairo_surface_create(lua_State *L) {
     luaL_getmetatable(L, "lualock.cairo_surface");
     lua_setmetatable(L, -2);
     
-    lua_pushvalue(L, -1);
-    luaL_ref(L, LUA_REGISTRYINDEX);
-    
     lsurface->layer = create_layer(luaL_checknumber(L, 3), luaL_checknumber(L, 4));
     lsurface->layer->x = luaL_checknumber(L, 1);
     lsurface->layer->y = luaL_checknumber(L, 2);
@@ -43,12 +40,26 @@ static int lualock_lua_cairo_surface_set_position(lua_State *L) {
     return 0;
 }
 
+static int lualock_lua_cairo_surface_resize(lua_State *L) {
+    lualock_surface_t *lsurface = luaL_checkudata(L, 1, "lualock.cairo_surface");
+    layer_t *layer = create_layer(luaL_checknumber(L, 2), luaL_checknumber(L, 3));
+    layer_t *oldlayer = lsurface->layer;
+    cairo_t *cr = cairo_create(layer->surface);
+    cairo_set_source_surface(cr, oldlayer->surface, 0, 0);
+    cairo_paint(cr);
+    cairo_destroy(cr);
+    replace_layer(oldlayer, layer);
+    lsurface->layer = layer;
+    return 0;
+}
+
 void lualock_lua_cairo_surface_init(lua_State *L) {
     const struct luaL_reg lualock_cairo_surface_lib[] =
     {
         { "show", lualock_lua_cairo_surface_show },
         { "get_surface", lualock_lua_cairo_surface_get_surface },
         { "set_position", lualock_lua_cairo_surface_set_position },
+        { "resize", lualock_lua_cairo_surface_resize },
         { NULL, NULL }
     };
     
